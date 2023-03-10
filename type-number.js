6fcd23222a45b00626f6bbc5d037c745ac961e19
Silamara <silmaramanaus@gmail.com>
2023-03-10 18:56:03 -0400

    exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,24 @@
+//tipo numero
+
+const meuNumero= 3;
+
+const primeiroNumero = 1;
+const segundoNumero =2;
+
+const operacaoMatematica = primeiroNumero / segundoNumero;
+
+ console.log("RESULTADO DA SOMA: " + operacaoMatematica)
+
+ //ponto flutuante
+
+ const mediaQuebrada = 3.5
+ const media2Quebrada = 2.3
+
+ const novaOperacao = mediaQuebrada + media2Quebrada; 
+
+ console.log("RESULTADO DA SEGUNDA SOMA COM PONTO FLUTU: " + novaOperacao);
+
+ //NAN -> NOT A NUMBER (NÃO É UM NÚMERO)
+
+ const sil = "sil";
+ console.log(sil * primeiroNumero);
---
+++ type-string.js
@@ -0,0 +1,9 @@
+const texto1 = "olá, mundo!";
+const texto2 = 'olá, mundo dnv';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros =  "34567";
+
+const citacao = 'Qual o nome dela? '; 
+// concatenação
+const MeuNome = "silmara"
+console.log (citacao + MeuNome)
